a6f3eb9e6b72ea1acdb55a0c4b1c0b52f8b360dd
Sebastian Hungerecker <sepp2k@googlemail.com>
2016-09-13 21:17:57 +0200

    Make util::error take variadic arguments

--- src/util.hh
+++ src/util.hh
@@ -5,12 +5,27 @@
 #include <string>
 #include <cstdlib>
 
-namespace util {
-    [[noreturn]] inline void error (const std::string& message)
+// This is a class with only static members rather than a namespace, so I can
+// define private helper functions
+class util {
+    // Print the given arguments to cerr
+    template<typename Head, typename ...Tail>
+    static void print(const Head& head, const Tail&... tail)
     {
-        std::cerr << message << std::endl;
+        std::cerr << head;
+        print(tail...);
+    }
+
+    static void print() {
+    }
+
+public:
+    template<typename ...T>
+    [[noreturn]] static void error(const T&... args)
+    {
+        print(args...);
+        std::cerr << std::endl;
         std::exit(1);
     }
-}
-
+};
 #endif
